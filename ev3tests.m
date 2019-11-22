47da2ca7a164d85074d50928722d86ead4f9a19a
Sarah Ferenczi <39970608+sarahf5676@users.noreply.github.com>
2019-11-21 19:28:25 -0700

    Update ev3tests.m

--- ev3tests.m
+++ ev3tests.m
@@ -1,24 +1,60 @@
-brick.SetColorMode(2, 4);  % Set Color Sensor connected to Port 1 to Color Code Mode
+global key;
+while 1
+    brick.MoveMotor('AB', 100)
+    touch = brick.TouchPressed(1);
+    brick.SetColorMode(2, 4);  
+    
+    color_rgb = brick.ColorRGB(2);  
 
-color_rgb = brick.ColorRGB(2);  % Get Color on port 1.
-touch = brick.TouchPressed(1);
-%print color of object
-
-fprintf("\tRed: %d\n", color_rgb(1));
-%fprintf("\tGreen: %d\n", color_rgb(2));
-%fprintf("\tBlue: %d\n", color_rgb(3));
-
-
-
-if color_rgb(1) < 200
-%pause(5)
-    brick.StopMotor('AB')
+    if touch
+        %disp("touch")
+        brick.MoveMotor('AB', -50)
+        pause(0.3)
+        brick.MoveMotor('A', -50)
+        brick.MoveMotor('B', 50)
+        pause(0.97)
+        
+    else
+        brick.MoveMotor('AB', 50)
+    end
+    disp(color_rgb)
+    if color_rgb(1) >= 60
+        brick.StopMotor('AB')
+        pause(2)
+    end
+    if color_rgb(3) >= 50
+        InitKeyboard();
+        while 1
+            pause(0.1);
+            switch key
+                case 'w'
+                    brick.MoveMotor('AB', 50);
+                case 's'
+                    brick.MoveMotor('AB', -50);
+                case 'a'
+                    brick.MoveMotor('A', -50);
+                    brick.MoveMotor('B', 50);
+                case 'd'
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', -50);
+                case 'e'
+                    brick.MoveMotor('C', -50)
+                case 'r'
+                    brick.MoveMotor('C', 50)
+                case 0
+                    brick.MoveMotor('ABC', 0);
+                case 'q'
+                    break;
+                    
+            end
+        end
+        CloseKeyboard();
+    end
+    if color_rgb(2) >= 30
+        brick.StopMotor()
+        brick.MoveMotor('C', 50)
+        pause(3)
+    end
 end
-if touch
-    brick.StopMotor('AB')
-    pause(0.1)
-    brick.MoveMotor('A', -50)
-    brick.MoveMotor('B', 50)
-    pause(1.5)
-    brick.StopMotor('AB')
-end
+
+
